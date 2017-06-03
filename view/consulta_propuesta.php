bf79e84fd79080a3f75d5880e5d0e7ac6d760670
JeimmyP <ruelasjeimmy@gmail.com>
2017-06-03 17:46:47 -0600

    03/06/2017
    actualizacion general.

---
+++ view/consulta_propuesta.php
@@ -0,0 +1,43 @@
+<div class="col-md-12 text-left">
+	<div class="col-md-2">
+	</div>
+	<div class="col-md-8">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title">Peticiones Recibidas</h3>
+			</div>
+			<div class="panel-body">	
+				<table class="table table-striped" id="mostrar_peticiones">
+					<thead>
+						Peticiones Recibidas
+					</thead>
+					<tbody>
+					</tbody>
+				</table>
+			</div>
+			
+		</div>
+	</div>
+	<div class="col-md-2">
+	</div>
+</div>
+<div class="modal fade" id="dlg_consulta_datos" tabindex="-1" role="dialog" aria-labelledby="dlg_consultar_datos" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" id="Cerrar"  aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+				<h5 id="nombre_propuesta"></h5>
+			</div>
+			<div class="modal-body">
+				<table class="table table-striped" id="mostrar_datos_completos">
+					<tbody>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script src="../js/consulta_propuesta.js"></script>
