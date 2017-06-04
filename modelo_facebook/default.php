75d4fcd074586fa8c9d5b11f37002cd4fe49a2be
Jose Pablo Domingo Aramburo Sanchez <jpdaramburos@concredito.com.mx>
2017-06-04 01:38:06 -0600

    Inicio del flujo de registro por facebook

--- modelo_facebook/default.php
+++ modelo_facebook/default.php
@@ -2,33 +2,23 @@
 //***********Si el usuario no tiene ninguna accion reciente registrada***********
 
 //Respuesta a facebook
-$url = 'https://graph.facebook.com/v2.9/me/messages?access_token='.$access_token;
-$message_to_reply = 'Su mensaje no pudo ser comprendido, por favor intente de nuevo.';
+$message_to_reply = 'Bienvenid@ al sistema de Culiacan inteligente, puede ver nuestras politicas de privacidad en jelousoft.com/privacidad \n \n Necesitamos que nos proveea de su numbre para continuar.';
 print $message_to_reply;
-//Inicializa cURL.
+
+//API Url
+$url = 'https://graph.facebook.com/v2.9/me/messages?access_token='.$access_token;
+//Initiate cURL.
 $ch = curl_init($url);
-//JSON a retornar
+//The JSON data.
 $jsonData = '{
-  "recipient":{
-    "id":"'.$sender.'"
-  },
-  "message":{
-  "text":"Hola espero se encuentre bien, ¿En que podemos ayudarle?",
-  "quick_replies":[
-    {
-      "content_type":"text",
-      "title":"Petición",
-      "payload":"peticion"
+    "recipient":{
+        "id":"'.$sender.'"
     },
-    {
-      "content_type":"text",
-      "title":"Mis peticiones",
-      "payload":"ver_estatus"
+    "message":{
+        "text":"'.$message_to_reply.'"
     }
-  ]
-}
 }';
 
-//Registra que no hay flujo actual
-$accion_pendiente_session = $facebook->introducir_ultimo_request($mysqli, $sender, "nothing");
+//Indica que espere un request para registrar correo
+$success = $facebook->introducir_ultimo_request($mysqli, $sender, "nothing");
 ?>
