d8252e830ad3ec085fb7ba9eac884dce5c88327f
JeimmyP <ruelasjeimmy@gmail.com>
2017-06-04 12:06:06 -0600

    1
    error

--- archivos_temporales/ciudadano.php
+++ archivos_temporales/ciudadano.php
@@ -13,7 +13,7 @@ class ciudadano
 												(?,?,?,now(),now())";
 
 		if ($stmt = $mysqli->prepare($query))
-
+				  
 				  //Asigna las variables para el query
 				  $stmt->bind_param("isi", $id, $nombre, $estatus);
 
@@ -102,33 +102,31 @@ class ciudadano
     return $respuesta;
 
     }
-
-		function almacenar_correo($mysqli, $correo_electronico )
-		{
-
-			$estatus = 1;
-
-			$query = "INSERT INTO `correo_electronico`(`id_correo_electronico`,
-													`correo_electronico`, `estatus`, `fecha_creacion`, `ultima_modificacion`)
-												VALUES
-													(NULL,?,?,now(),now())";
-
-			if ($stmt = $mysqli->prepare($query))
-					  {
-					  //Asigna las variables para el query
-					  $stmt->bind_param("ii", $correo_electronico, $estatus);
-
-					  //Ejecuta el query
-					  $stmt->execute();
-
-					  $id_ciudadano = $stmt->insert_id;
-
-					  //Cierra el query
-					  $stmt->close();
-					  }
-
-					return $id_ciudadano;
-
+	
+	function informacion_ciudano_por_id($mysqli, $id_ciudadano){
+		$estatus = 1;
+		$datos_ciudadano = new array();
+		$query = "SELECT ciudadano.nombre, correo_electronico.correo_electronico, telefono.telefono FROM ciudadano
+					INNER JOIN ciudadano_correo_electronico ON ciudadano_correo_electronico.id_ciudadano = ciudadano.id_ciudadano
+					INNER JOIN correo_electronico ON correo_electronico.id_correo_electronico = ciudadano_correo_electronico.id_correo_electronico
+					INNER JOIN ciudadano_telefono ON ciudadano_telefono.id_ciudadano = ciudadano.id_ciudadano
+					INNER JOIN telefono ON telefono.id_telefono = ciudadano_telefono.id_telefono
+					WHERE ciudadano.id_ciudadano = ? AND ciudadano.estatus = ? AND correo_electronico.estatus = ? AND telefono.estatus = ?";
+		if ($stmt = $mysqli->prepare($query)){
+			$stmt->bind_param( "iiii", $id_ciudadano, $estatus, $estatus, $estatus);
+			$stmt->bind_result($nombre, $telefono, $correo);
+			
+			while ($stmt->fetch())
+			{
+				//Cierra el query
+				$stmt->close();
+				
+				$datos_ciudadano = Array (
+					"nombre" => $nombre, 
+					"telefono" => $telefono, 
+					"correo" => $correo
+				);
+			}
 		}
 		return($datos_ciudadano);
 	}
