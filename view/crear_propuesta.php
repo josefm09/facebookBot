27089518099c226be07ae650a746a831ce7bdab3
AlexisDos <usuario_wp21@hotmail.com>
2017-06-04 01:31:26 -0600

    Algo

--- view/crear_propuesta.php
+++ view/crear_propuesta.php
@@ -1,6 +1,5 @@
 <link href="../css/dropzone.min.css" type="text/css" rel="stylesheet"/>
 		<div class="col-md-8">
-			<h3><span class='nombreSistema'>Nueva Propuesta <small class="numero_formulario">PARTE 1</small></span></h3>
 		</div>
 		<div class="col-md-2">
 			<div class="form-group">
@@ -21,45 +20,23 @@
 					<div class="col-md-12">
 						<div class = "panel panel-default">
 							<div class = "panel-heading">
-								<h3 class = "panel-title">
-									Problematica
-								</h3>
+								<h3 class = "panel-title text-right nombreSistema">Nueva Propuesta <small class="numero_formulario">PARTE 1</small></h3>
+								<h3 class = "panel-title">Problematica</h3>
+								
 							</div>
 							<div class = "panel-body">
 								<div class="row">
 									<div class="col-md-1"></div>
-									<div class="form-group col-md-10">
-										<label for="txt_titulo_propuesta">Titulo</label>
-										<input type="text" class="form-control" id="txt_titulo_propuesta" autofocus="">
+									<div class="col-md-10">
+										<label for="txt_titulo_propuesta">Titulo de propuesta</label>
+										<input class="form-control" type="text" id="txt_titulo_propuesta"/>
 									</div>
 									<div class="col-md-1"></div>
 								</div>
 								<div class="row">
 									<div class="col-md-1"></div>
-									<div class="form-group col-md-10">
-										<label for="txt_problema">Problema</label>
-										<textarea type="text" class="form-control" id="txt_problema" rows="5" cols="40" placeholder="Proporcione el problema en cuatiion"/>
-									</div>
-									<div class="col-md-1"></div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				
-				<div class="row formulario_2">
-					<div class="col-md-12">
-						<div class = "panel panel-default">
-							<div class = "panel-heading">
-								<h3 class = "panel-title">
-									Tema/Subtema
-								</h3>
-						   </div>
-							<div class = "panel-body">
-								<div class="row">
-									<div class="col-md-2"></div>
-									<div class="col-md-8">
-										<label for="txt_id_tema">Se relaciona la problematica con eel tema</label><br/>
+									<div class="col-md-10">
+										<label for="txt_id_tema"><br/>Relación de la problematica con el tema</label><br/>
 										<div class="col-md-6">
 											<div class="form-group">
 												<label for="txt_id_tema">Tema</label>
@@ -77,8 +54,32 @@
 											</div>
 										</div>
 									</div>
-									<div class="col-md-2"></div>
+									<div class="col-md-1"></div>
 								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+				<div class="row formulario_2">
+					<div class="col-md-12">
+						<div class = "panel panel-default">
+							<div class = "panel-heading">
+								<h3 class = "panel-title text-right nombreSistema">Nueva Propuesta <small class="numero_formulario"></small></h3>
+								<h3 class = "panel-title">
+									Problematica
+								</h3>
+						   </div>
+							<div class = "panel-body">
+								<div class="row">
+									<div class="col-md-1"></div>
+									<div class="form-group col-md-10">
+										<label for="txt_problema">Problema</label>
+										<textarea type="text" class="form-control" id="txt_problema" rows="5" cols="40" placeholder="Proporcione el problema en cuatiion"/>
+									</div>
+									<div class="col-md-1"></div>
+								</div>
+								
 							</div>
 						</div>
 					</div>
@@ -88,6 +89,7 @@
 					<div class="col-md-12">
 						<div class = "panel panel-default">
 							<div class = "panel-heading">
+								<h3 class = "panel-title text-right nombreSistema">Nueva Propuesta <small class="numero_formulario"></small></h3>
 								<h3 class = "panel-title">
 									Propuesta
 								</h3>
@@ -108,25 +110,22 @@
 					<div class="col-md-12">
 						<div class = "panel panel-default">
 							<div class = "panel-heading">
+							<h3 class = "panel-title text-right nombreSistema">Nueva Propuesta <small class="numero_formulario"></small></h3>
 								<h3 class = "panel-title">
 									Adjunto
 								</h3>
-						   </div>
+						    </div>
 							<div class = "panel-body">
-								<div class = "panel panel-default">
-									<div class = "panel-body">
-										<div class="col-md-3"></div>
-										<div class="col-md-6">
-											<h4><strong>Imagen</strong></h4>
-											<form class="dropzone" id="upload_imgaen_propuesta" enctype="multipart/form-data" method="post">
-												<div class="fallback">
-													<input name="file" type="file"/>
-												</div>
-											</form>
+								<div class="col-md-4"></div>
+								<div class="col-md-4">
+									<h4><strong>Imagen</strong></h4>
+									<form class="dropzone" id="upload_imgaen_propuesta" enctype="multipart/form-data" method="post">
+										<div class="fallback">
+											<input name="file" type="file"/>
 										</div>
-										<div class="col-md-3"></div>
-									</div>
+									</form>
 								</div>
+								<div class="col-md-4"></div>
 							</div>
 						</div>
 					</div>
