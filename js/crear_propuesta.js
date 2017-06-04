11d8df9b7382199244d3cdd47650f2fe1f731e9b
AlexisDos <usuario_wp21@hotmail.com>
2017-06-03 21:17:41 -0600

    arreglo de id de subtema

--- js/crear_propuesta.js
+++ js/crear_propuesta.js
@@ -145,7 +145,7 @@ $(document).ready(function(){
 				$(data).each(function(i,v){
 					
 					$('#txt_id_subtema').append(
-						'<option id="'+v.id_sub_tema+'">'+ v.sub_tema + '</option>'
+						'<option value="'+v.id_sub_tema+'">'+ v.sub_tema + '</option>'
 					);
 				});
 			},
@@ -155,6 +155,7 @@ $(document).ready(function(){
 			}
 		});		
 	});
+	
 	
 	
 	function almacenar_propuesta(){
@@ -177,7 +178,7 @@ $(document).ready(function(){
 					var estatus_request = (data["estatus_request"]);//Notifica el estatus del query
 					var respuesta_servidor = (data["respuesta_servidor"]);//Mensaje a mostrar al usuario
 
-					//Si el servidor retorna un error
+					// Si el servidor retorna un error
 					if(estatus_request == "error")
 					{
 						$('#modal_respuesa_servidor_error').modal('show');
@@ -185,20 +186,20 @@ $(document).ready(function(){
 						$("#reset_password_usuario").focus(); //Pasa el focus al punto de inicio del formulario
 					}
 
-					//Si no hay errores
+					// Si no hay errores
 					if(estatus_request == "success")
 					{
 						$('#modal_respuesa_servidor_success').modal('show');
 						document.getElementById('respuesa_servidor_success').innerHTML = respuesta_servidor;
 
-						//Cierra el modal despues de 3 segundos
+						// Cierra el modal despues de 3 segundos
 						setTimeout(function(){
 							$("#modal_respuesa_servidor_success").modal('hide');
 						}, 3000);
 						cargar_vista('crear_cliente');//Recarga la vista para limpiar todo el form
 					}
 				},
-				//Si el request falla genera mensajes de errores de posibles eventos comunes
+				// Si el request falla genera mensajes de errores de posibles eventos comunes
 				error:function(x, desc, e) {
 					console.log(x);
 					console.log("Details: " + desc + "\nError:" + e);
