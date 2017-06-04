4a5f5f4c166b0134694f38c088539e18cfbdfe35
AlexisDos <usuario_wp21@hotmail.com>
2017-06-04 10:39:38 -0600

    voto

---
+++ modelo_no_auth/gestion_voto_ciudadano.php
@@ -0,0 +1,19 @@
+<?PHP
+
+	class voto_ciudadano(){
+		
+		function insertar_voto($mysqli, $id_propuesta, $id_ciudadano, $voto){
+			$estatus=1;
+			$query = "INSERT INTO `propuesta_votacion_ciudadano`(`id_propuesta_votacion_ciudadano`, `id_propuesta`, `id_ciudadano`, `votacion`, `estatus`, `fecha_creacion`, `ultima_modificacion`)
+						VALUES(NULL, ?,?,?,?,now(),now())";
+			$stm = $mysqli->prepare($query);
+			$stm->bind_param("iiii",$id_propuesta, $id_ciudadano, $voto);
+			$stm->execute();
+			$id_propuesta_votacion_ciudadano=-$mysqli->insert_id;
+			$stm->close();
+			return ($id_propuesta_votacion_ciudadano);
+		}
+		
+	}
+
+?>
---
+++ modelo_no_auth/insertar_voto_ciudadano.php
@@ -0,0 +1,13 @@
+<?PHP
+	require '../modelo_no_auth/gestion_voto_ciudadano.php';
+	
+	$voto_ciudadano = new voto_ciudadano();
+	
+	$id_ciudadano = $REQUEST['id_ciudadano'];
+	$id_propuesta = $REQUEST['id_propuesta'];
+	$voto = $REQUEST['voto'];
+	
+	$id_propuesta_voto_ciudadano = $voto_ciudadano->insertar_voto($mysqli, $id_propuesta, $id_ciudadano, $voto);
+	
+	echo($id_propuesta_voto_ciudadano);	
+?>
